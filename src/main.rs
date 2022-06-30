5aa12207ad635bbe33eaba161812db49563a56f1
Kingdream <kingdream123@163.com>
2022-06-30 23:17:59 +0800

    button

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow};
+use gtk::{Application, ApplicationWindow, Button};
 
-const APP_ID: &str = "org.gtk_rs.HelloWorld2";
+const APP_ID: &str = "org.gtk_rs.HelloWorld";
 
 fn main() {
     // Create a new application
@@ -15,10 +15,26 @@ fn main() {
 }
 
 fn build_ui(app: &Application) {
-    // Create a window and set the title
+    // Create a button with label and margins
+    let button = Button::builder()
+        .label("Press me!")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    // Connect to "clicked" signal of `button`
+    button.connect_clicked(move |button| {
+        // Set the label to "Hello World!" after the button has been clicked on
+        button.set_label("Hello World!");
+    });
+
+    // Create a window
     let window = ApplicationWindow::builder()
         .application(app)
         .title("My Rust App")
+        .child(&button)
         .build();
 
     // Present window
